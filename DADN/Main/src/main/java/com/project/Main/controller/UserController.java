77f25f0c0aa148d0294c090dfc429cf2d1c110c5
duongCyberclone123 <duong.maibk106khmt@hcmut.edu.vn>
2025-04-07 12:22:39 +0700

    Init

---
+++ DADN/Main/src/main/java/com/project/Main/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.project.Main.controller;
+
+import com.project.Main.dto.request.UserCreationRequest;
+import com.project.Main.home.User;
+import com.project.Main.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/users")
+    User createUser(@RequestBody UserCreationRequest request) {
+        return userService.createUser(request);
+    }
+}
+
