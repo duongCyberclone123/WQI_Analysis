551805049264e9c679764bf1f9e549a4be014968
Nguyen Thuan <kanivan2203@gmail.com>
2025-04-27 16:56:51 +0700

    fix: path auth.controller

--- backend/auth_service/src/routes/auth.routes.js
+++ backend/auth_service/src/routes/auth.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("../controllers/auth.controller");
+const controller = require("../controllers/auth.controllers");
 
 router.post("/register", controller.register);
 router.post("/verify", controller.verifyCode);
